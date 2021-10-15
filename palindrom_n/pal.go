e9b18e8a7b25b35575b9e77e92fa990421ff3e93
Дмитрий Яковлев <yad_94@mail.ru>
2021-10-15 23:54:14 +0900

    palindrom case added

--- palindrom_n/pal.go
+++ palindrom_n/pal.go
@@ -4,9 +4,23 @@ import (
 	"fmt"
 )
 
+func pal(st string) bool {
+	result1 := []byte{}
+	result2 := []byte{}
+	for i := len(st) - 1; i >= 0; i-- {
+		if st[i] >= 65 && st[i] <= 90 || st[i] >= 97 && st[i] <= 122 {
+			result1 = append(result1, st[i])
+		}
+	}
+	for i := 0; i <= len(st)-1; i++ {
+		if st[i] >= 65 && st[i] <= 90 || st[i] >= 97 && st[i] <= 122 {
+			result2 = append(result2, st[i])
+		}
+	}
+
+	return string(result1) == string(result2)
+}
+
 func main() {
-	var a int
-	barr := []string{}
-	a = 3
-	fmt.Println(barr, a)
+	fmt.Println(pal("qwertytrwq"))
 }
